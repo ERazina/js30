b7a584a84ac09c7e1e73c12150993b9fb096fb1c
Elina Razina <razina_elina@mail.ru>
2019-06-15 22:33:36 +0300

    дщпшс шы вщту

--- js-drum/script.js
+++ js-drum/script.js
@@ -1,3 +1,21 @@
-window.addEventListener('keydown', function(e){
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     console.log(e.keyCode);
-});
+    if(!audio){return;}
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('highlight');
+}
+function removeTransition(e){
+    if(e.propertyName !== 'transform'){
+        return;
+    }
+    console.log(e);
+    this.classList.remove('highlight');
+}
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    // }key.addEventListener('transition', removeTransition));
+    window.addEventListener('keydown', playSound);
